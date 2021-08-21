d6f770d30c5ce7134a1fadffa458747ae3855a9a
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-08-20 21:46:21 -0400

    SinglePane dashboard layout component

---
+++ src/routes/dashboard/components/SinglePane.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let id: string = ""
+</script>
+
+<div class="container" {id}>
+	<slot />
+</div>
+
+<style lang="scss">
+	@import "../../../mixins/mixins";
+
+	.container {
+		margin-bottom: 3rem;
+		display: flex;
+		font-size: 1.8rem;
+		min-height: 10vh;
+		max-height: inherit;
+		height: 100%;
+		padding: 2rem;
+		background-color: var(--color-background2);
+		border-radius: var(--border-md);
+		width: 100%;
+
+		@include respond-below(lg) {
+			font-size: 1.6rem;
+		}
+
+		@include respond-below(sm) {
+			flex-direction: column;
+		}
+	}
+</style>
