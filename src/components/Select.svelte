70f82b2c390e614a75ebe036d53d278300f4c4f0
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-09-04 14:52:05 -0400

    Basic Select component

---
+++ src/components/Select.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte"
+
+	export let name
+
+	const dispatch = createEventDispatcher()
+
+	function notifyChange(e) {
+		dispatch("change", e.target)
+	}
+</script>
+
+<div class="select-wrapper">
+	<select class="select" {name} on:blur={notifyChange} on:change={notifyChange}>
+		<slot />
+	</select>
+</div>
+
+<style lang="scss">
+	.select-wrapper {
+		height: 100%;
+		width: 100%;
+	}
+
+	.select {
+		height: 100%;
+		width: 100%;
+		border: none;
+		background: none;
+		color: var(--color-text2);
+	}
+</style>
