c65158bba6a66df389df6855079606a2368b5f29
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-09-13 16:13:31 -0400

    Manual PermissionsCheck component

---
+++ src/components/PermissionCheck.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	import { onMount } from "svelte"
+	import {
+		checkFlag,
+		FLAG_ADMINISTRATOR,
+		FLAG_SUPER_ADMIN,
+		getFlag,
+	} from "../permissions/permissions"
+
+	export let permissions: BigInt
+	export let oneOf: string[] = []
+	export let allOf: string[] = []
+	export let adminBypass: boolean = true
+
+	const needOneOf = oneOf.length > 0
+	const needAllOf = allOf.length > 0
+
+	let oneOfSatisfied = false
+	if (!needOneOf) {
+		oneOfSatisfied = true
+	}
+
+	let allOfSatisfied = false
+	if (!needAllOf) {
+		allOfSatisfied = true
+	}
+
+	console.log("PermissionCheck permissions", permissions)
+
+	if (permissions) {
+		// Check if the user is an admin or super admin. If they are, satisfy permission requirements without check
+		if (
+			checkFlag(permissions, getFlag(FLAG_SUPER_ADMIN)) ||
+			(adminBypass && checkFlag(permissions, getFlag(FLAG_ADMINISTRATOR)))
+		) {
+			oneOfSatisfied = true
+			allOfSatisfied = true
+		}
+
+		// Check one of perms
+		if (!oneOfSatisfied) {
+			for (const flagName of oneOf) {
+				const flag = getFlag(flagName)
+
+				if (checkFlag(permissions, flag)) {
+					oneOfSatisfied = true
+					break
+				}
+			}
+		}
+
+		// Check all of perms
+		if (!allOfSatisfied) {
+			let satisfied = true
+
+			for (const flagName of allOf) {
+				const flag = getFlag(flagName)
+
+				if (!checkFlag(permissions, flag)) {
+					satisfied = false
+					break
+				}
+			}
+
+			allOfSatisfied = satisfied
+		}
+	}
+
+	let show = oneOfSatisfied && allOfSatisfied
+</script>
+
+{#if show}
+	<slot />
+{/if}
