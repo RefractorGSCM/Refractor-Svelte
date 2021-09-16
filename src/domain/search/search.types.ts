6308472a635738c2360267631d7961f613c2dacf
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-09-16 16:30:18 -0400

    Player searching, result display
    Pagination is next

--- src/domain/search/search.types.ts
+++ src/domain/search/search.types.ts
@@ -1,6 +1,20 @@
+import type { Player } from "../player/player.types"
+
 export type PlayerSearchBody = {
 	term: string
 	type: string
 	limit: number
 	offset: number
+}
+
+export type PlayerSearchResult = {
+	id: string
+	platform: string
+	name: string
+	last_seen: Date
+}
+
+export type PlayerSearchResults = {
+	total: number
+	results: PlayerSearchResult[]
 }
