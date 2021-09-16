d0ac493fcd71696552808fe722f35faa8d4980e7
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-09-16 16:05:28 -0400

    Started player records page layout

--- src/components/DurationPicker.svelte
+++ src/components/DurationPicker.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { createEventDispatcher } from "svelte"
 
-	import Select from "./Select.svelte"
+	import Select from "./SimpleSelect.svelte"
 
 	export let name
 	export let value: number = 0
