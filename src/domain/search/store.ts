c9fe4973d59332374700af7242cd4c55d7178c16
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-09-22 18:21:09 -0400

    Seach store infraction search function

--- src/domain/search/store.ts
+++ src/domain/search/store.ts
@@ -1,6 +1,11 @@
-import { errorToast } from "../../utils/toast"
+import { errorToast, successToast } from "../../utils/toast"
 import api from "./api"
-import type { PlayerSearchBody, PlayerSearchResults } from "./search.types"
+import type {
+	InfractionSearchBody,
+	InfractionSearchResults,
+	PlayerSearchBody,
+	PlayerSearchResults,
+} from "./search.types"
 
 export async function searchPlayers(
 	body: PlayerSearchBody,
@@ -11,5 +16,39 @@ export async function searchPlayers(
 		return data.payload as PlayerSearchResults
 	} catch (err) {
 		errorToast("Could not run player search")
+	}
+}
+
+type infractionSearchRes = {
+	results: InfractionSearchResults
+	success: boolean
+	errors?: object
+}
+
+export async function searchInfractions(
+	body: InfractionSearchBody,
+): Promise<infractionSearchRes> {
+	try {
+		const { data } = await api.searchInfractions(body)
+
+		return {
+			results: data.payload as InfractionSearchResults,
+			success: true,
+		}
+	} catch (err) {
+		const { data } = err.response
+
+		if (data.errors) {
+			return {
+				results: null,
+				success: false,
+				errors: data.errors,
+			}
+		}
+
+		return {
+			results: null,
+			success: false,
+		}
 	}
 }
