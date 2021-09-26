53d4955bbf853566e6cbf40ab729eb943ac3313d
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-09-26 14:16:41 -0400

    Get all games

--- src/domain/game/store.ts
+++ src/domain/game/store.ts
@@ -1,14 +1,19 @@
 import { Writable, writable } from "svelte/store"
+import { errorToast } from "../../utils/toast"
 import api from "./api"
 import type { Game } from "./game.types"
 
 export const allGames: Writable<Game[]> = writable([])
-export const selectedGame: Writable<Game> = writable(null)
 
-export async function getAllGames() {
+export async function getAllGames(): Promise<Game[]> {
 	try {
-		const { data } = await api.getEnabledGames()
+		const { data } = await api.getAllGames()
 
-		console.log(data)
-	} catch (err) {}
+		allGames.set(data.payload as Game[])
+
+		return data.payload as Game[]
+	} catch (err) {
+		const { data } = err.response
+		errorToast(data.message ? data.message : "Could not get all games")
+	}
 }
