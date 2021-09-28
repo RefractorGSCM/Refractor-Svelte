f639a2cd2c9aacc59d8e1cb78a6942754a53cc46
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-09-28 18:18:48 -0400

    Websocket reconnection routine

--- src/domain/websocket/store.ts
+++ src/domain/websocket/store.ts
@@ -1,4 +1,6 @@
+import { toast } from "@zerodevx/svelte-toast"
 import { writable } from "svelte/store"
+import { errorToast, successToast } from "../../utils/toast"
 import { addChatMessage } from "../chat/store"
 import { addPlayerToServer, removePlayerFromServer } from "../player/store"
 import { setServerStatus } from "../server/store"
@@ -8,16 +10,77 @@ const messageStore = writable("")
 let socket: WebSocket = null
 let pingInterval
 
+let reconnectInterval
+let tryingToReconnect = false
+let reconnectToastIds: number[] = []
+
 export function openWebsocketConnection() {
 	socket = new WebSocket(`${WS_ROOT}`)
 
 	socket.addEventListener("open", () => {
+		if (tryingToReconnect) {
+			// if this connection was initialized by the reconnect routine then cleanup the
+			// reconnect routine.
+
+			clearInterval(reconnectInterval)
+			tryingToReconnect = false
+			for (const toastId of reconnectToastIds) {
+				toast.pop(toastId)
+			}
+			reconnectToastIds = []
+
+			successToast("Websocket reconnected")
+		}
+
 		console.log("Websocket connection established")
 	})
 
-	socket.addEventListener("close", () => {
+	socket.addEventListener("close", (info) => {
 		console.log("Websocket connection closed")
 		clearInterval(pingInterval)
+
+		console.log(info) // code 1006?
+
+		if (info.wasClean || tryingToReconnect) {
+			// if the disconnect was clean, this was done intentionally.
+			return
+		}
+		tryingToReconnect = true
+
+		const options = {
+			theme: {
+				"--toastBackground": "var(--color-danger)",
+			},
+			duration: 99999999,
+			dismissable: false,
+			progress: 0,
+			initial: -1000000,
+			reversed: undefined,
+			intro: undefined,
+		}
+
+		reconnectToastIds.push(
+			toast.push(
+				"Refractor may not behave properly while the websocket is disconnected.",
+				options,
+			),
+		)
+
+		reconnectToastIds.push(
+			toast.push(
+				"Websocket connection closed. Attempting to reconnect...",
+				options,
+			),
+		)
+
+		reconnectInterval = setInterval(() => {
+			if (!tryingToReconnect) {
+				// if tryingToReconnect is false, then reconnection was succesful so this interval should stop running
+				return
+			}
+
+			openWebsocketConnection()
+		}, 2000)
 	})
 
 	socket.addEventListener("message", (wsmsg) => {
