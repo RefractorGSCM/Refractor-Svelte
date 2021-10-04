6f9d64a2eccab1f9bc003777333ce1da53ad51f8
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-10-04 01:58:16 -0400

    Refractor settings refactor

---
+++ src/routes/dashboard/FlaggedWords.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Heading from "../../components/Heading.svelte"
+	import Container from "./components/Container.svelte"
+</script>
+
+<Container>
+	<Heading type="title">Settings: Flagged Words</Heading>
+
+	<div class="flagged-words" />
+</Container>
