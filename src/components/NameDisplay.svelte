1629447e62b484fb467008956be8429b7bd3441a
Sniddunc <6261352+sniddunc@users.noreply.github.com>
2021-10-21 14:50:51 -0400

    NameDisplay for unknown users

--- src/components/NameDisplay.svelte
+++ src/components/NameDisplay.svelte
@@ -2,15 +2,25 @@
 	import { onMount } from "svelte"
 	import { writable } from "svelte/store"
 	import type { Group } from "../domain/group/group.types"
-	import { getUserPrimaryGroup } from "../domain/group/store"
-	import { decimalToHex } from "../utils/color"
+	import { baseGroup, getUserPrimaryGroup } from "../domain/group/store"
+	import { decimalToHex, hexToDecimal } from "../utils/color"
 	import Spinner from "./Spinner.svelte"
 
 	export let userId
 
 	let group = writable(null as Group)
 	onMount(async () => {
-		group.set(await getUserPrimaryGroup(userId))
+		if (userId) {
+			group.set(await getUserPrimaryGroup(userId))
+		} else {
+			group.set({
+				id: -2,
+				color: hexToDecimal("424242"),
+				name: "",
+				permissions: BigInt(0),
+				position: -1,
+			})
+		}
 	})
 </script>
 
