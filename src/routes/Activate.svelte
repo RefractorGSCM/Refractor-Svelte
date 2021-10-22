c6b6c5ef1ddb55f4a08b038e428734e709ceb78f
dsnidr <6261352+dsnidr@users.noreply.github.com>
2021-10-21 20:29:50 -0400

    Log out option on email verification page

--- src/routes/Activate.svelte
+++ src/routes/Activate.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { AUTH_ROOT } from "../env"
+	import { AUTH_ROOT, KRATOS_ROOT } from "../env"
 
 	import Button from "../components/Button.svelte"
 	import Heading from "../components/Heading.svelte"
@@ -27,8 +27,12 @@
 		<div class="resend-notice">
 			If you didn't receive a verification email, please make sure that you're
 			using the correct email and check your spam folder. If you still don't
-			have a verification email, <a href={`${AUTH_ROOT}/k/verify`}>click here</a
-			> to receive a new one.
+			have a verification email, <a href={`${AUTH_ROOT}/verify`}>click here</a> to
+			receive a new one.
+		</div>
+
+		<div class="logout">
+			<a href={`${KRATOS_ROOT}/self-service/browser/flows/logout`}>Log out</a>
 		</div>
 	</div>
 </div>
@@ -101,5 +105,10 @@
 			font-size: 14rem;
 			color: var(--color-primary);
 		}
+	}
+
+	.logout a {
+		text-decoration: underline;
+		color: var(--color-primary);
 	}
 </style>
